be820aea6972ac8bd5a6ed6d1012e27f229465df
rkeepers <rkeepers@sparksonline.net>
2019-09-22 11:37:38 -0700

    add nohup to command now that Raspbian Buster has fixed background task quirk. Also refined choices to quit once stream is chosen

--- stream.py
+++ stream.py
@@ -4,7 +4,7 @@
 # Streaming Audio Selector
 # This uses  VLC media player to play selected Online Audio Streams
 # 2019 rkeepers
-# SOV: 0.01
+# SOV: 0.02
 
 
 import sys
@@ -26,20 +26,24 @@ def main():
 	3) Groove Salad
 	4) Illinois Street Lounge
 	5) NPR
+        q) Kill stream and quit
 	x) Exit
 	""")
 	while choice != '':	
 		choice=raw_input('Choose the stream you would like to play : ')
 		if choice=="1":
-			stream = 'https://somafm.com/suburbsofgoa.pls &'
+			stream = 'https://somafm.com/suburbsofgoa.pls'
 		elif choice=="2":
-			stream = 'http://somafm.com/secretagent.pls &'
+			stream = 'http://somafm.com/secretagent.pls'
 		elif choice=="3":
-			stream = 'http://somafm.com/groovesalad.pls &'
+			stream = 'http://somafm.com/groovesalad.pls'
 		elif choice=="4":	
-			stream = 'http://somafm.com/illstreet.pls &'		
+			stream = 'http://somafm.com/illstreet.pls'		
 		elif choice=="5":	
-			stream = 'https://live.wostreaming.net/direct/kplu-newsjazzaac-64?source=link &'	
+			stream = 'https://live.wostreaming.net/direct/kplu-newsjazzaac-64?source=link'	
+		elif choice=="q":
+			subprocess.Popen(['killall', '-9', 'vlc'], stdout=FNULL, stderr=subprocess.STDOUT)
+			sys.exit()
 		elif choice=="x":
 			sys.exit()
 		elif choice =="":
@@ -48,7 +52,7 @@ def main():
 			sys.exit()	
 		elif choice !="":
 			print(" Invalid Choice .. exiting")
-			sys.exit()	
+			#sys.exit()	
 		
 		# kill any running vlc procs
 		subprocess.Popen(['killall', '-9', 'vlc'], stdout=FNULL, stderr=subprocess.STDOUT)
@@ -56,7 +60,7 @@ def main():
 		time.sleep(0.5)
 
 		# open our stream with vlc and supress output of system info	
-		subprocess.Popen(['cvlc', stream], stdout=FNULL, stderr=subprocess.STDOUT)
+		subprocess.Popen(['nohup', 'cvlc', stream], stdout=FNULL, stderr=subprocess.STDOUT)
 
 	
 	# back to main
